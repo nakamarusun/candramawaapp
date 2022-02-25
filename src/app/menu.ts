6930b0cb0605f57678adc6f222c1c37ff0e25d92
Jason Christian H <nakamarujc@gmail.com>
2022-02-26 00:16:25 +0700

    Added sidebar colors, achanged search button to use angular material thing

--- src/app/menu.ts
+++ src/app/menu.ts
@@ -1,18 +1,18 @@
 export interface Menu {
-  name: string,
-  href: string,
-  icon: string,
+  name: string;
+  href: string;
+  icon: string;
 }
 
 export const menus: Menu[] = [
   {
-    name: "Home",
-    href: "#",
-    icon: ""
+    name: 'Home',
+    href: '/',
+    icon: 'home',
   },
   {
-    name: "Products",
-    href: "#",
-    icon: ""
+    name: 'Produk',
+    href: '/produk',
+    icon: 'color_lens',
   },
-]
+];
